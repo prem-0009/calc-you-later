bccba7d5dab94fc00185cff8bdfa0daf482a255f
prem <prem.sherpa@codeimmersives.com>
2020-06-23 14:36:22 -0400

    front end calculator

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,6 +1,22 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculate1 = require('./calculate-back-end');
+
+const num1 = Number(getInput(0));
+const num2 = Number(getInput(2));
+const operator = getInput(1); 
+
+
+
+//function
+const calculateIt = calculate1(num1, num2, operator);
+
+
+
+
+//stretch
+
 
 
 
